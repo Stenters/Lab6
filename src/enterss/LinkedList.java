393f544eef83638a8ca305d57f049deb024044cd
enterss <entersstuart@gmail.com>
2018-04-23 11:24:11 -0500

    initial commit

---
+++ src/enterss/LinkedList.java
@@ -0,0 +1,261 @@
+/*
+ * CS2852
+ * Spring 2018
+ * Lab 6 - Recursion
+ * Name: 
+ * Created: 4/13/2018
+ */
+package enterss;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * A partial implementation of a singly linked list to be used
+ * to practice implementing recursive methods.
+ *
+ * @param <E> The type of objects stored in the list
+ */
+public class LinkedList<E> implements List<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+
+    /**
+     * Inner class that describes a node in the singly linked list.
+     */
+    private static class Node<E> {
+        E value;
+        Node<E> next;
+
+        /**
+         * Constructs a node with a specified value and reference to the next node
+         * @param value Value of the element to be stored in the node
+         * @param next Reference to the next element in the singly linked list
+         */
+        private Node(E value, Node<E> next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        /**
+         * Constructs a node with a specified value
+         * @param value Value of the element to be stored in the node
+         */
+        private Node(E value) {
+            this(value, null);
+        }
+    }
+
+    /**
+     * Constructs a list containing the elements of the specified collection, in
+     * the order they are returned by the collection's iterator.
+     * @param collection the collection whose elements are to be placed into this list
+     * @NullPointerException if the specified collection is <tt>null</tt>
+     */
+    public LinkedList(Collection<? extends E> collection) {
+        Iterator<? extends E> itr = collection.iterator();
+        if(itr.hasNext()) {
+            head = new Node(itr.next());
+            tail = head;
+        }
+        Node<E> walker = head;
+        while(itr.hasNext()) {
+            walker.next = new Node(itr.next());
+            walker = walker.next;
+        }
+        tail = walker;
+    }
+
+    /**
+     * Returns the number of elements in this list
+     * @return the number of elements in this list
+     */
+    @Override
+    public int size() {
+        return size(head);
+    }
+
+    private int size(Node<E> current) {
+        if (current == null){
+            return 0;
+        }else {
+            return 1 + (size(current.next));
+        }
+    }
+
+    /**
+     * Returns true if this list contains the specified element.
+     * <p></p>
+     * More formally, returns <tt>true</tt> if and only if this list contains
+     * at least one element <tt>e</tt> such that <tt>(o==null ? e==null : o.equals(e))</tt>.
+     * @param target element whose presence in this list is to be tested
+     * @return true if this list contains the specified element
+     */
+    @Override
+    public boolean contains(Object target) {
+        return contains(target,head);
+    }
+
+    private boolean contains(Object target, Node<E> current) {
+        return current != null && (current == target || contains(target,current.next));
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or <tt>-1</tt> if this list does not contain the element.
+     * <p></p>
+     * More formally, returns the lowest index <tt>i</tt> such that
+     * <tt>(o==null ? get(i)==null : o.equals(get(i)))</tt>,
+     * or <tt>-1</tt> if there is no such index.
+     * @param target element to search for
+     * @return the index of the first occurrence of the specified element in this list,
+     *         or <tt>-1</tt> if this list does not contain the element
+     */
+    @Override
+    public int indexOf(Object target) {
+        return indexOf(target,head);
+    }
+
+    private int indexOf(Object target, Node<E> current) {
+        if (current == null){
+            return -1;
+        }
+        if (current.equals(target)){
+            return 0;
+        } else {
+            return 1 + indexOf(target, current.next);
+        }
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (index<0 || index>=size())
+     */
+    @Override
+    public E get(int index){
+        return get(index,head);
+    }
+
+
+    private E get(int index, Node<E> current) {
+        if(index<0 || index > size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        }
+        try {
+            if (index == 0){
+                return current.value;
+            } else {
+                return get(--index, current.next);
+            }
+        } catch(NullPointerException e) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        }
+    }
+
+    @Override
+    public E set(int index, E element) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean add(E element) {
+        Node walker = head;
+        while (walker.next != null){
+            walker = walker.next;
+        }
+        walker.next = new Node(element);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object target) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void add(int index, E element) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public E remove(int index) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+}
---
+++ src/enterss/Tester.java
@@ -0,0 +1,266 @@
+/*
+ * CS2852
+ * Spring 2017
+ * Lab 6 - Recursion
+ * Name: 
+ * Created: 3/30/2017
+ */
+package enterss;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import enterss.ArrayList;
+import enterss.LinkedList;
+
+/**
+ * Tests the ArrayList and LinkedList implementations for the
+ * CS2852 Lab 6 assignment.
+ */
+public class Tester {
+    private final static String[] EMPTY_STRINGS = {};
+    private final static String[] STRINGS1 = {"this"};
+    private final static String[] STRINGS2 = {"this", "will"};
+    private final static String[] STRINGS3 = {"this", "will", "be"};
+    private final static String[] STRINGS4 = {"this", "will", "be", "interesting"};
+    private final static String[] STRINGS5 = {"this", null, "will", "be", "interesting"};
+
+    private final static int[] EMPTY_INTS = {};
+    private final static int[] INTS1 = {-1};
+    private final static int[] INTS2 = {-1, 2};
+    private final static int[] INTS3 = {-1, 2, 8};
+    private final static int[] INTS4 = {-1, 2, 8, Integer.MAX_VALUE};
+
+    /**
+     * Calls all of the test methods
+     * @param ignored Not used
+     */
+    public static void main(String[] ignored) {
+        String description = "ArrayList constructor collection of Strings of size: ";
+        test(description + 0, stringConstructor(EMPTY_STRINGS));
+        test(description + 1, stringConstructor(STRINGS1));
+        test(description + 2, stringConstructor(STRINGS2));
+        test(description + 3, stringConstructor(STRINGS3));
+        test(description + 4, stringConstructor(STRINGS4));
+        test(description + 5, stringConstructor(STRINGS5));
+
+        description = "ArrayList constructor collection of Integers of size: ";
+        test(description + 0, intConstructor(EMPTY_INTS));
+        test(description + 1, intConstructor(INTS1));
+        test(description + 2, intConstructor(INTS2));
+        test(description + 3, intConstructor(INTS3));
+        test(description + 4, intConstructor(INTS4));
+
+        description = "ArrayList.contains method checker";
+        test(description, arrayListContainsTest());
+//        description = "ArrayList.indexOf method checker";
+//        test(description, arrayListIndexOfTest())
+//        ;description = "LinkedList.contains method checker";
+//        test(description, linkedListContainsTest())
+//        ;description = "LinkedList.indexOf method checker";
+//        test(description, linkedListIndexOfTest());
+//        description = "LinkedList.get method checker";
+//        test(description, linkedListGetTest());
+//        description = "LinkedList.size method checker";
+//        test(description, linkedListSizeTest());
+    }
+
+    /**
+     * Displays an error if the test fails
+     * @param description Description of the test
+     * @param passed true if the test passed
+     */
+    private static void test(String description, boolean passed) {
+        if(!passed) {
+            System.out.println("Test failed: " + description);
+        }
+    }
+
+    private static boolean arrayListContainsTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+
+        List<String> emptyList = new ArrayList<>(input);
+        input.add(STRINGS1.toString());
+        List<String> oneList = new ArrayList<>(input);
+        Collections.addAll(input,STRINGS5);
+        List<String> list = new ArrayList<>(input);
+        boolean passed = list.size() == STRINGS5.length;
+
+        if (!list.contains("this")){
+            passed = false;
+        } else if (!list.contains("interesting")){
+            passed = false;
+        } else if (emptyList.contains(null)){
+            passed = false;
+        } else if (list.contains("hello")){
+            passed = false;
+        } else if (!oneList.contains(STRINGS1.toString())){
+            passed = false;
+        } else if (!list.contains(null)){
+            passed = false;
+        } else if (list.contains("")){
+            passed = false;
+        }
+
+        return passed;
+    }
+
+    private static boolean arrayListIndexOfTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+
+        List<String> emptyList = new ArrayList<>(input);
+        input.add(STRINGS1.toString());
+        List<String> oneList = new ArrayList<>(input);
+        Collections.addAll(input,STRINGS5);
+        input.add("will");
+        List<String> list = new ArrayList<>(input);
+
+        boolean passed = list.size() == STRINGS5.length;
+
+        if (list.indexOf("this") != 0){
+            passed = false;
+        } else if (list.indexOf("interesting") != 4){
+            passed = false;
+        } else if (emptyList.indexOf(null) != -1){
+            passed = false;
+        } else if (list.indexOf("hello") != -1){
+            passed = false;
+        } else if (oneList.indexOf(STRINGS1.toString()) != 0){
+            passed = false;
+        } else if (list.indexOf("will") != 2) {
+            passed = false;
+        }
+
+        return passed;
+    }
+
+    private static boolean linkedListContainsTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+
+        List<String> emptyList = new LinkedList<>(input);
+        input.add(STRINGS1.toString());
+        List<String> oneList = new LinkedList<>(input);
+        Collections.addAll(input,STRINGS5);
+        input.add("will");
+        List<String> list = new LinkedList<>(input);
+
+        boolean passed = list.size() == STRINGS5.length;
+
+        if (!list.contains("interesting")){
+            passed = false;
+        } else if (!list.contains("this")) {
+            passed = false;
+        } else if (!oneList.contains(STRINGS1.toString())){
+            passed = false;
+        } else if (emptyList.contains(null)){
+            passed = false;
+        } else if (list.contains("hello")){
+            passed = false;
+        } else if (!list.contains(null)){
+            passed = false;
+        } else if (list.contains("")){
+            passed = false;
+        }
+
+        return passed;
+    }
+
+    private static boolean linkedListIndexOfTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+
+        List<String> emptyList = new LinkedList<>(input);
+        input.add(STRINGS1.toString());
+        List<String> oneList = new LinkedList<>(input);
+        Collections.addAll(input,STRINGS5);
+        input.add("will");
+        List<String> list = new LinkedList<>(input);
+
+        boolean passed = list.size() == STRINGS5.length;
+
+        if (list.indexOf("this") != 0){
+            passed = false;
+        } else if (list.indexOf("hello") != -1){
+            passed = false;
+        } else if (list.indexOf("interesting") != 4){
+            passed = false;
+        } else if (emptyList.indexOf(null) != -1){
+            passed = false;
+        } else if (oneList.indexOf(STRINGS1.toString()) != 0){
+            passed = false;
+        } else if (list.indexOf("will") != 2) {
+            passed = false;
+        }
+        return passed;
+    }
+
+    private static boolean linkedListGetTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+
+        Collections.addAll(input,STRINGS5);
+        input.add("will");
+        List<String> list = new LinkedList<>(input);
+
+        boolean passed = list.size() == STRINGS5.length;
+
+        if (!list.get(0).equals("this")){
+            passed = false;
+        } else if (list.get(1) != (null)){
+            passed = false;
+        }
+        return passed;
+    }
+
+    private static boolean linkedListSizeTest() {
+        Collection<String> input = new java.util.ArrayList<>();
+        List<String> list = new LinkedList<>(input);
+
+        int size = 0;
+        boolean passed = list.size() == size;
+        for (String s : STRINGS5) {
+            list.add(s);
+            size++;
+            if (list.size() != size) {
+                passed = false;
+            }
+        }
+
+        list.remove(null);
+        size--;
+        if (list.size() != size){
+            passed = false;
+         }
+
+        return passed;
+    }
+
+
+    /**
+     * Tests the constructor that accepts a collection of elements.
+     * After the ArrayList is instantiated, the size is compared with
+     * the size of the collection.
+     * @param strings An array of Strings to be added to the ArrayList
+     * @return true if test passed
+     */
+    private static boolean stringConstructor(String[] strings) {
+        Collection<String> input = new java.util.ArrayList<>();
+        Collections.addAll(input, strings);
+        List<String> list = new ArrayList<>(input);
+        return list.size()==input.size();
+    }
+
+    /**
+     * Tests the constructor that accepts a collection of elements.
+     * After the ArrayList is instantiated, the size is compared with
+     * the size of the collection.
+     * @param numbers An array of ints to be added to the ArrayList
+     * @return true if test passed
+     */
+    private static boolean intConstructor(int[] numbers) {
+        Collection<Integer> input = new java.util.ArrayList<>();
+        for(int number : numbers) {
+            input.add(number);
+        }
+        List<Integer> list = new ArrayList<>(input);
+        return list.size()==input.size();
+    }
+}
